605b0fdeb811726ea825f668e63eb2af1dd81dea
Tyler Bacong <tbacong@cpp.edu>
2025-03-11 18:14:56 -0700

    Added more tests for subtract(), multiply(), and divide()

--- test_calculator.c
+++ test_calculator.c
@@ -32,18 +32,52 @@ void test_add_zero(void) {
 	TEST_ASSERT_EQUAL(0, add(0, 0));    // Expect 0 + 0 = 0
 }
 
-void test_subtract(void) {
-	TEST_ASSERT_EQUAL(2, subtract(5, 3));  // Expect 5 - 3 = 2
+void test_subtract_positive_numbers(void) {
+	TEST_ASSERT_EQUAL(1, subtract(3, 2)); // We expect 3 - 2 to be 1
 }
 
-void test_multiply(void) {
-	TEST_ASSERT_EQUAL(15, multiply(5, 3)); // 5 * 3 = 15
+void test_subtract_positive_and_negative_numbers(void) {
+	TEST_ASSERT_EQUAL(9, subtract(5, -4));  // Expect 5 - (-4) = 9
+ }
+ 
+void test_subtract_negative_numbers(void) {
+	TEST_ASSERT_EQUAL(1, subtract(-3, -4)); // Expect -3 - -4 = -7
 }
  
-void test_divide(void) {
+void test_subtract_zero(void) {
+	TEST_ASSERT_EQUAL(10, subtract(10, 0));  // Expect 10 - 0 = 10
+	TEST_ASSERT_EQUAL(0, subtract(0, 0));    // Expect 0 - 0 = 0
+}
+
+void test_multiply_positive_numbers(void) {
+	TEST_ASSERT_EQUAL(6, multiply(3, 2)); // We expect 3 * 2 to be 6
+}
+
+void test_multiply_positive_and_negative_numbers(void) {
+	TEST_ASSERT_EQUAL(-20, multiply(5, -4));  // Expect 5 * (-4) = -20
+ }
+ 
+void test_multiply_negative_numbers(void) {
+	TEST_ASSERT_EQUAL(12, multiply(-3, -4)); // Expect -3 * -4 = 12
+}
+ 
+void test_multiply_zero(void) {
+	TEST_ASSERT_EQUAL(0, multiply(10, 0));  // Expect 10 * 0 = 0
+	TEST_ASSERT_EQUAL(0, multiply(0, 0));    // Expect 0 * 0 = 0
+}
+ 
+void test_divide_positive_numbers(void) {
 	TEST_ASSERT_EQUAL(2, divide(6, 3));    // 6 / 3 = 2
 }
+
+void test_divide_positive_and_negative_numbers(void) {
+	TEST_ASSERT_EQUAL(-5, divide(10, -2));  // Expect 10 * (-2) = -5
+}
  
+void test_divide_negative_numbers(void) {
+	TEST_ASSERT_EQUAL(5, divide(-10, -2)); // Expect -10 / -2 = 5
+}
+
 void test_divide_by_zero(void) {
 	TEST_ASSERT_EQUAL(0, divide(5, 0));    // Define behavior for division by zero
 }
@@ -55,9 +89,17 @@ int main(void) {
 	RUN_TEST(test_add_positive_and_negative_numbers);
 	RUN_TEST(test_add_negative_numbers);
 	RUN_TEST(test_add_zero);
-	RUN_TEST(test_subtract);
-	RUN_TEST(test_multiply);
-	RUN_TEST(test_divide);
+	RUN_TEST(test_multiply_positive_numbers);
+	RUN_TEST(test_multiply_positive_and_negative_numbers);
+	RUN_TEST(test_multiply_negative_numbers);
+	RUN_TEST(test_multiply_zero);
+	RUN_TEST(test_subtract_positive_numbers);
+	RUN_TEST(test_subtract_positive_and_negative_numbers);
+	RUN_TEST(test_subtract_negative_numbers);
+	RUN_TEST(test_subtract_zero);
+	RUN_TEST(test_divide_positive_numbers);
+	RUN_TEST(test_divide_positive_and_negative_numbers);
+	RUN_TEST(test_divide_negative_numbers);
 	RUN_TEST(test_divide_by_zero);
 	return UNITY_END();
 }
